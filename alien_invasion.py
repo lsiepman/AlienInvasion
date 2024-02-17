e6c8ef63283b2b3389b4d63b77606795850ecddc
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2024-02-17 21:54:20 +0100

    moved settings to settings file

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,19 +1,23 @@
 import sys
 import pygame
+from settings import Settings
 
 
 def run_game():
+
+    # create instancce of settings class
+    s = Settings()
+
     # Init game and create screen object
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    screen = pygame.display.set_mode(s.screen_size)
     pygame.display.set_caption("Alien Invasion")
-    bg_colour = (24, 27, 97)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        screen.fill(bg_colour)
+        screen.fill(s.bg_colour)
         pygame.display.flip()
 
 
---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    "A class to store global Alien Invasion settings"
+
+    def __init__(self) -> None:
+        # Screen settings
+        self.screen_size = (1200, 800)
+        self.bg_colour = (24, 27, 97)
